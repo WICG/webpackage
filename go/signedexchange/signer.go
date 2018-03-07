07f3c2754410afe5da3a17abd215f60d27226d0a
Kouhei Ueno <kouhei@chromium.org>
2018-03-08 03:19:50 +0900

    gen-signedexchange: Remove 32 spaces from signed message

--- go/signedexchange/signer.go
+++ go/signedexchange/signer.go
@@ -41,22 +41,17 @@ func (s *Signer) serializeSignedMessage(e *Exchange) ([]byte, error) {
 	// attacks when TLS certificates are used to sign manifests." [spec text]
 	var buf bytes.Buffer
 
-	// "1. A string that consists of octet 32 (0x20) repeated 64 times." [spec text]
-	for i := 0; i < 64; i++ {
-		buf.WriteByte(0x20)
-	}
-
-	// "2. A context string: the ASCII encoding of "HTTP Exchange"." [spec text]
+	// "1. A context string: the ASCII encoding of "HTTP Exchange"." [spec text]
 	buf.WriteString("HTTP Exchange")
 
-	// "3. A single 0 byte which serves as a separator." [spec text]
+	// "2. A single 0 byte which serves as a separator." [spec text]
 	buf.WriteByte(0)
 
-	// "4. The bytes of the canonical CBOR serialization (Section 3.5) of a CBOR map
+	// "3. The bytes of the canonical CBOR serialization (Section 3.5) of a CBOR map
 	// mapping:" [spec text]
 	mes := []*cbor.MapEntryEncoder{}
 
-	// "4.1. If certSha256 is set: The text string "certSha256" to the byte string
+	// "3.1. If certSha256 is set: The text string "certSha256" to the byte string
 	// certSha256." [spec text]
 	if b := certSha256(s.Certs); len(b) > 0 {
 		mes = append(mes,
@@ -67,25 +62,25 @@ func (s *Signer) serializeSignedMessage(e *Exchange) ([]byte, error) {
 	}
 
 	mes = append(mes,
-		// "4.2. The text string "validityUrl" to the byte string value of validityUrl."
+		// "3.2. The text string "validityUrl" to the byte string value of validityUrl."
 		// [spec text]
 		cbor.GenerateMapEntry(func(keyE *cbor.Encoder, valueE *cbor.Encoder) {
 			keyE.EncodeTextString("validityUrl")
 			valueE.EncodeByteString([]byte(s.ValidityUrl.String()))
 		}),
-		// "4.3. The text string "date" to the integer value of date."
+		// "3.3. The text string "date" to the integer value of date."
 		// [spec text]
 		cbor.GenerateMapEntry(func(keyE *cbor.Encoder, valueE *cbor.Encoder) {
 			keyE.EncodeTextString("date")
 			valueE.EncodeInt(s.Date.Unix())
 		}),
-		// "4.4. The text string "expires" to the integer value of expires."
+		// "3.4. The text string "expires" to the integer value of expires."
 		// [spec text]
 		cbor.GenerateMapEntry(func(keyE *cbor.Encoder, valueE *cbor.Encoder) {
 			keyE.EncodeTextString("expires")
 			valueE.EncodeInt(s.Expires.Unix())
 		}),
-		// "4.5. The text string "headers" to the CBOR representation (Section
+		// "3.5. The text string "headers" to the CBOR representation (Section
 		// 3.4) of exchange's headers."
 		cbor.GenerateMapEntry(func(keyE *cbor.Encoder, valueE *cbor.Encoder) {
 			keyE.EncodeTextString("headers")
