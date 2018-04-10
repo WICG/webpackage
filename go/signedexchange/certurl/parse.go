508edba3191bff692aadcea9d17d9655a8b511ad
Hajime Hoshi <hajimehoshi@gmail.com>
2018-04-10 17:38:17 +0900

    gen-certurl: Follow the latest specification of certurl (OCSP and SCT)

--- go/signedexchange/certurl/parse.go
+++ go/signedexchange/certurl/parse.go
@@ -5,26 +5,16 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
-)
 
-func writeHead(w *bytes.Buffer, n int, size int) error {
-	// encoding/binary's Write is not available here
-	// since the size is not necessarily a power of 2.
-	for i := 0; i < size; i++ {
-		if err := w.WriteByte(byte(n >> (8 * uint(size-i-1)))); err != nil {
-			return err
-		}
-	}
-	return nil
-}
+	"github.com/WICG/webpackage/go/signedexchange/cbor"
+)
 
 // CertificateMessageFromPEM parses a PEM formatted content to a certUrl content.
 // See https://wicg.github.io/webpackage/draft-yasskin-http-origin-signed-responses.html for the spec.
-func CertificateMessageFromPEM(pemFileContent []byte) ([]byte, error) {
+func CertificateMessageFromPEM(pemFileContent, ocspFileContent, sctFileContent []byte) ([]byte, error) {
 	b := pemFileContent
 
 	entries := []*x509.Certificate{}
-	totalLength := 0
 	for {
 		block, rest := pem.Decode(b)
 		if block == nil && len(rest) > 0 {
@@ -36,7 +26,6 @@ func CertificateMessageFromPEM(pemFileContent []byte) ([]byte, error) {
 		}
 
 		entries = append(entries, c)
-		totalLength += len(c.Raw)
 
 		if len(rest) == 0 {
 			break
@@ -45,58 +34,38 @@ func CertificateMessageFromPEM(pemFileContent []byte) ([]byte, error) {
 	}
 
 	buf := &bytes.Buffer{}
+	enc := cbor.NewEncoder(buf)
 
-	// enum {
-	//     X509(0),
-	//     RawPublicKey(2),
-	//     (255)
-	// } CertificateType;
-	//
-	// struct {
-	//     select (certificate_type) {
-	//     case RawPublicKey:
-	//         /* From RFC 7250 ASN.1_subjectPublicKeyInfo */
-	//         opaque ASN1_subjectPublicKeyInfo<1..2^24-1>;
-	//
-	//     case X509:
-	//         opaque cert_data<1..2^24-1>;
-	//     };
-	//     Extension extensions<0..2^16-1>;
-	// } CertificateEntry;
-	//
-	// struct {
-	//     opaque certificate_request_context<0..2^8-1>;
-	//     CertificateEntry certificate_list<0..2^24-1>;
-	// } Certificate;
-	//
-	// https://tools.ietf.org/html/draft-ietf-tls-tls13-23#section-4.4.2
-	const (
-		certificateRequestContextHeadLength = 1
-		certificateListHeadLength           = 3
-		certDataHeadLength                  = 3
-		extensionsHeadLength                = 2
-	)
-
-	// certificate_request_context is always empty, so just write the length '0' in 1 byte.
-	// See https://wicg.github.io/webpackage/draft-yasskin-http-origin-signed-responses.html#rfc.section.3.6
-	if err := writeHead(buf, 0, certificateRequestContextHeadLength); err != nil {
+	if err := enc.EncodeArrayHeader(len(entries) + 1); err != nil {
 		return nil, err
 	}
-
-	if err := writeHead(buf, totalLength+(certDataHeadLength+extensionsHeadLength)*len(entries), certificateListHeadLength); err != nil {
+	if err := enc.EncodeTextString("\U0001F4DC\u26D3"); err != nil {
 		return nil, err
 	}
-
-	for _, entry := range entries {
-		if err := writeHead(buf, len(entry.Raw), certDataHeadLength); err != nil {
-			return nil, err
+	for i, entry := range entries {
+		mes := []*cbor.MapEntryEncoder{
+			cbor.GenerateMapEntry(func(keyE *cbor.Encoder, valueE *cbor.Encoder) {
+				keyE.EncodeTextString("cert")
+				valueE.EncodeByteString(entry.Raw)
+			}),
 		}
-		if _, err := buf.Write(entry.Raw); err != nil {
-			return nil, err
+		if i == 0 {
+			if ocspFileContent != nil {
+				mes = append(mes,
+					cbor.GenerateMapEntry(func(keyE *cbor.Encoder, valueE *cbor.Encoder) {
+						keyE.EncodeTextString("ocsp")
+						valueE.EncodeByteString(ocspFileContent)
+					}))
+			}
+			if sctFileContent != nil {
+				mes = append(mes,
+					cbor.GenerateMapEntry(func(keyE *cbor.Encoder, valueE *cbor.Encoder) {
+						keyE.EncodeTextString("sct")
+						valueE.EncodeByteString(sctFileContent)
+					}))
+			}
 		}
-		// TODO: OCSP Status and SignedCertificateTimestamps extensions will be
-		// needed to be included.
-		if err := writeHead(buf, 0, extensionsHeadLength); err != nil {
+		if err := enc.EncodeMap(mes); err != nil {
 			return nil, err
 		}
 	}
