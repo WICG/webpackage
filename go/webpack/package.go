41a40eac714bafac7f0c821292b4396ced60dc43
Hajime Hoshi <hajimehoshi@gmail.com>
2019-01-15 15:30:24 +0900

    Remove go/webpack

--- go/webpack/package.go
+++
@@ -1,9 +0,0 @@
-// Package webpack parses and serializes Web Packages in text and binary formats.
-//
-// Web Packages are defined in https://github.com/WICG/webpackage.
-package webpack
-
-type Package struct {
-	manifest Manifest
-	parts    []*PackPart
-}
