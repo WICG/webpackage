d91f24c194b45e448f489bc133a6e485dbb15f6a
Kunihiko Sakamoto <ksakamoto@chromium.org>
2019-11-06 10:31:05 +0900

    js/bundle: Add tests (#515)
    Also bumps package version to 0.0.2.

---
+++ js/bundle/tests/decoder_test.js
@@ -0,0 +1,68 @@
+const wbn = require('../lib/wbn');
+const fs = require('fs');
+const path = require('path');
+
+describe('Bundle', () => {
+  const bundleBuffer = (() => {
+    const builder = new wbn.BundleBuilder('https://example.com/');
+    builder.setManifestURL('https://example.com/manifest.json');
+    builder.addExchange(
+      'https://example.com/',
+      200,
+      { 'Content-Type': 'text/plain' },
+      'Hello, world!'
+    );
+    builder.addExchange(
+      'https://example.com/ja/',
+      200,
+      { 'Content-Type': 'text/plain', 'Content-Language': 'ja' },
+      'こんにちは世界'
+    );
+    return builder.createBundle();
+  })();
+
+  it('has expected fields', () => {
+    const b = new wbn.Bundle(bundleBuffer);
+    expect(b.primaryURL).toBe('https://example.com/');
+    expect(b.manifestURL).toBe('https://example.com/manifest.json');
+    expect(b.urls).toEqual(['https://example.com/', 'https://example.com/ja/']);
+  });
+
+  describe('getResponse', () => {
+    it('returns Response with expected fields', () => {
+      const b = new wbn.Bundle(bundleBuffer);
+      const resp1 = b.getResponse('https://example.com/');
+      const resp2 = b.getResponse('https://example.com/ja/');
+      expect(resp1.status).toBe(200);
+      expect(resp2.status).toBe(200);
+      expect(resp1.headers).toEqual({ 'content-type': 'text/plain' });
+      expect(resp2.headers).toEqual({
+        'content-type': 'text/plain',
+        'content-language': 'ja',
+      });
+      expect(resp1.body.toString('utf-8')).toBe('Hello, world!');
+      expect(resp2.body.toString('utf-8')).toBe('こんにちは世界');
+    });
+
+    it('throws if URL is not found', () => {
+      const b = new wbn.Bundle(bundleBuffer);
+      expect(() =>
+        b.getResponse('https://example.com/nonexistent')
+      ).toThrowError();
+    });
+  });
+
+  it('parses pregenerated bundle', () => {
+    const buf = fs.readFileSync(path.resolve(__dirname, 'testdata/hello.wbn'));
+    const b = new wbn.Bundle(buf);
+    expect(b.primaryURL).toBe('https://example.com/hello.html');
+    expect(b.manifestURL).toBe(null);
+    expect(b.urls).toEqual(['https://example.com/hello.html']);
+    const resp = b.getResponse('https://example.com/hello.html');
+    expect(resp.status).toBe(200);
+    expect(resp.headers['content-type']).toBe('text/html; charset=utf-8');
+    expect(resp.body.toString('utf-8')).toBe(
+      '<html>Hello, Web Bundle!</html>\n'
+    );
+  });
+});
---
+++ js/bundle/tests/encoder_test.js
@@ -0,0 +1,65 @@
+const wbn = require('../lib/wbn');
+const CBOR = require('cbor');
+
+describe('Bundle Builder', () => {
+  const primaryURL = 'https://example.com/';
+  const defaultHeaders = { 'Content-Type': 'text/plain' };
+  const defaultContent = 'Hello, world!';
+  const invalidURLs = [
+    '',
+    'ftp://example.com/',
+    'https://example.com/#fragment',
+    'https://user:pass@example.com/',
+    'relative/url',
+  ];
+
+  it('builds', () => {
+    const builder = new wbn.BundleBuilder(primaryURL);
+    builder.addExchange(primaryURL, 200, defaultHeaders, defaultContent);
+    const buf = builder.createBundle();
+    // Just checks the result is a valid CBOR array.
+    expect(CBOR.decode(buf)).toBeInstanceOf(Array);
+  });
+
+  it('rejects invalid primary URLs', () => {
+    invalidURLs.forEach(url => {
+      expect(() => new wbn.BundleBuilder(url)).toThrowError();
+    });
+  });
+
+  describe('addExchange', () => {
+    it('rejects invalid URLs', () => {
+      const builder = new wbn.BundleBuilder(primaryURL);
+      invalidURLs.forEach(url => {
+        expect(() =>
+          builder.addExchange(url, 200, defaultHeaders, defaultContent)
+        ).toThrowError();
+      });
+    });
+
+    it('requires content-type for non-empty resources', () => {
+      const builder = new wbn.BundleBuilder(primaryURL);
+      expect(() =>
+        builder.addExchange(primaryURL, 200, {}, defaultContent)
+      ).toThrowError();
+      builder.addExchange(primaryURL, 200, {}, ''); // This is accepted
+    });
+  });
+
+  describe('setManifestURL', () => {
+    it('rejects invalid URLs', () => {
+      const builder = new wbn.BundleBuilder(primaryURL);
+      invalidURLs.forEach(url => {
+        expect(() => builder.addManifestURL(url)).toThrowError();
+      });
+    });
+
+    it('rejects double call', () => {
+      const builder = new wbn.BundleBuilder(primaryURL);
+      builder.setManifestURL('https://example.com/manifest.json');
+      expect(() =>
+        builder.setManifestURL('https://example.com/manifest.json')
+      ).toThrowError();
+    });
+  });
+});
