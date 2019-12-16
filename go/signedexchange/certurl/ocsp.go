b3244a4e6004bfe8c2d175de779b9d2bab23e18a
Tomoki Nakagawa <49511064+tomokinat@users.noreply.github.com>
2019-12-16 15:45:21 +0900

    make certurl.CreateOCSPRequest compliant with rfc5019 (#543)
    * make certurl.CreateOCSPRequest compliant with rfc5019
    
    * add preferGET flag
    
    * fix typo and construction of the URL for GET
    
    * add command-line flag preferGET
    
    * add test-cert-long.pem and unskip the corresponding test
    
    * construct GET url by sinple string concat

--- go/signedexchange/certurl/ocsp.go
+++ go/signedexchange/certurl/ocsp.go
@@ -3,14 +3,16 @@ package certurl
 import (
 	"bytes"
 	"crypto/x509"
+	"encoding/base64"
 	"fmt"
 	"golang.org/x/crypto/ocsp"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
-func CreateOCSPRequest(certs []*x509.Certificate) (*http.Request, error) {
+func CreateOCSPRequest(certs []*x509.Certificate, preferGET bool) (*http.Request, error) {
 	if len(certs) < 2 {
 		return nil, fmt.Errorf("Could not fetch OCSP response: Issuer certificate not found")
 	}
@@ -18,24 +20,40 @@ func CreateOCSPRequest(certs []*x509.Certificate) (*http.Request, error) {
 	if len(cert.OCSPServer) == 0 {
 		return nil, fmt.Errorf("Could not fetch OCSP response: No OCSP responder field")
 	}
-	ocspUrl := cert.OCSPServer[0]
+	ocspURL := cert.OCSPServer[0]
 	issuer := certs[1]
 
 	ocspRequest, err := ocsp.CreateRequest(cert, issuer, &ocsp.RequestOptions{})
 	if err != nil {
 		return nil, err
 	}
-	request, err := http.NewRequest("POST", ocspUrl, bytes.NewReader(ocspRequest))
-	if err != nil {
-		return nil, err
+
+	// Use GET for small requests per https://tools.ietf.org/html/rfc5019#section-5.
+
+	// Use QueryEscape here instead of PathEscape to escape not only '/' but also '+' and '='
+	// to align with the exmaple in https://tools.ietf.org/html/rfc5019#section-5.
+	// Note that the string to be escaped doesn't contain other symbols as it is base64 encoded.
+	getURL := ocspURL + "/" + url.QueryEscape(base64.StdEncoding.EncodeToString(ocspRequest))
+	if preferGET && len(getURL) <= 255 {
+		request, err := http.NewRequest("GET", getURL, nil)
+		if err != nil {
+			return nil, err
+		}
+		request.Header.Add("Accept", "application/ocsp-response")
+		return request, nil
+	} else {
+		request, err := http.NewRequest("POST", ocspURL, bytes.NewReader(ocspRequest))
+		if err != nil {
+			return nil, err
+		}
+		request.Header.Add("Content-Type", "application/ocsp-request")
+		request.Header.Add("Accept", "application/ocsp-response")
+		return request, nil
 	}
-	request.Header.Add("Content-Type", "application/ocsp-request")
-	request.Header.Add("Accept", "application/ocsp-response")
-	return request, nil
 }
 
-func FetchOCSPResponse(certs []*x509.Certificate) ([]byte, error) {
-	request, err := CreateOCSPRequest(certs)
+func FetchOCSPResponse(certs []*x509.Certificate, preferGET bool) ([]byte, error) {
+	request, err := CreateOCSPRequest(certs, preferGET)
 	if err != nil {
 		return nil, err
 	}
--- go/signedexchange/certurl/ocsp_test.go
+++ go/signedexchange/certurl/ocsp_test.go
@@ -1,33 +1,66 @@
 package certurl_test
 
 import (
+	"github.com/WICG/webpackage/go/signedexchange"
+	. "github.com/WICG/webpackage/go/signedexchange/certurl"
 	"golang.org/x/crypto/ocsp"
 	"io/ioutil"
 	"testing"
-
-	. "github.com/WICG/webpackage/go/signedexchange/certurl"
-	"github.com/WICG/webpackage/go/signedexchange"
 )
 
-func TestOCSP(t *testing.T) {
-	expectedResponderURL := "http://ocsp.digicert.com"
+func TestCreateOCSPRequestSmall(t *testing.T) {
+	expectedRequestURL := "http://ocsp.digicert.com/MFEwTzBNMEswSTAJBgUrDgMCGgUABBTPJvUY%2Bsl%2Bj4yzQuAcL2oQno5fCgQUUWj%2FkK8CB3U8zNllZGKiErhZcjsCEA5kxfvCNq3hSxcq60HHjLA%3D"
 
 	pem, err := ioutil.ReadFile("test-cert.pem")
 	if err != nil {
-		t.Errorf("Cannot read test-cert.pem: %v", err)
-		return
+		t.Fatalf("Cannot read test-cert.pem: %v", err)
 	}
 	certs, err := signedexchange.ParseCertificates(pem)
 	if err != nil {
-		t.Errorf("Cannot parse test-cert.pem: %v", err)
-		return
+		t.Fatalf("Cannot parse test-cert.pem: %v", err)
 	}
 
-	req, err := CreateOCSPRequest(certs)
+	req, err := CreateOCSPRequest(certs, true)
 	if err != nil {
-		t.Errorf("CreateOCSPRequest failed: %v", err)
-		return
+		t.Fatalf("CreateOCSPRequest failed: %v", err)
 	}
+
+	if req.Method != "GET" {
+		t.Errorf("OCSP request Method:\ngot: %q,\nwant: %q", req.Method, "GET")
+	}
+	if req.URL.String() != expectedRequestURL {
+		t.Errorf("OCSP request URL:\ngot: %q,\nwant: %q", req.URL.String(), expectedRequestURL)
+	}
+	if req.Header.Get("Accept") != "application/ocsp-response" {
+		t.Errorf("OCSP request Accept header:\ngot: %q,\nwant: %q", req.Header.Get("Accept"), "application/ocsp-response")
+	}
+}
+
+func TestCreateOCSPRequestLarge(t *testing.T) {
+	// $ openssl genrsa -out ca.key 2048
+	// $ openssl req -x509 -new -nodes -sha256 -key ca.key -out ca.pem -subj '/CN=example.com/O=Test/C=US'
+	// $ openssl ecparam -out test-cert-long.key -name prime256v1 -genkey
+	// $ openssl req -new -sha256 -key test-cert-long.key -out test-cert-long.csr -subj /CN=example.com
+	// $ openssl x509 -req -in test-cert-long.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out test-cert-long-leaf.pem \
+	//     -extfile <(echo -e "1.3.6.1.4.1.11129.2.1.22 = ASN1:NULL\nsubjectAltName=DNS:example.com\nauthorityInfoAccess=OCSP;URI:http://very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-long-ocsp.example.com")
+	// $ openssl test-cert-long-leaf.pem ca.pem > test-cert-long.pem
+
+	expectedResponderURL := "http://very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-long-ocsp.example.com"
+
+	pem, err := ioutil.ReadFile("test-cert-long.pem")
+	if err != nil {
+		t.Fatalf("Cannot read test-cert-long.pem: %v", err)
+	}
+	certs, err := signedexchange.ParseCertificates(pem)
+	if err != nil {
+		t.Fatalf("Cannot parse test-cert-long.pem: %v", err)
+	}
+
+	req, err := CreateOCSPRequest(certs, true)
+	if err != nil {
+		t.Fatalf("CreateOCSPRequest failed: %v", err)
+	}
+
 	if req.Method != "POST" {
 		t.Errorf("OCSP request Method:\ngot: %q,\nwant: %q", req.Method, "POST")
 	}
@@ -41,11 +74,13 @@ func TestOCSP(t *testing.T) {
 		t.Errorf("OCSP request Accept header:\ngot: %q,\nwant: %q", req.Header.Get("Accept"), "application/ocsp-response")
 	}
 
+	if req.Body == nil {
+		t.Fatalf("Empty request body")
+	}
 	defer req.Body.Close()
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		t.Errorf("Cannot read request body: %v", err)
-		return
+		t.Fatalf("Cannot read request body: %v", err)
 	}
 	if _, err = ocsp.ParseRequest(body); err != nil {
 		t.Errorf("Cannot parse request body as an OCSP request: %v", err)
