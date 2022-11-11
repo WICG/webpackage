5c4b20f2f1416186cdda38ee6a985a7b7a975ef1
Sonja Laurila <laurilasonja@gmail.com>
2022-11-11 15:07:22 +0100

    Create IntegrityBlock class and toCBOR method for it

--- js/sign/tests/integrityblock_test.js
+++ js/sign/tests/integrityblock_test.js
@@ -1,7 +1,9 @@
 import * as wbnSign from '../lib/wbn-sign.js';
+import * as constants from '../lib/constants.js';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as crypto from 'crypto';
+import * as cborg from 'cborg';
 import url from 'url';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
@@ -23,5 +25,20 @@ describe('Integrity Block', () => {
     for (const keyType of ['rsa', 'dsa', 'ec', 'ed448', 'x25519', 'x448']) {
       expect(() => initSignerWithTestWebBundleAndKeys(keyType)).toThrowError();
     }
+  });
+
+  it('encodes an empty integrity block CBOR correctly.', () => {
+    const integrityBlock = new wbnSign.IntegrityBlock();
+    const cbor = integrityBlock.toCBOR();
+
+    expect(cbor).toEqual(
+      Uint8Array.from(Buffer.from('8348F09F968BF09F93A6443162000080', 'hex'))
+    );
+
+    const decoded = cborg.decode(cbor);
+    expect(decoded.length).toEqual(3);
+    expect(decoded[0]).toEqual(constants.INTEGRITY_BLOCK_MAGIC);
+    expect(decoded[1]).toEqual(constants.VERSION_B1);
+    expect(decoded[2]).toEqual([]);
   });
 });
